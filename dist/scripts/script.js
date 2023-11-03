65dc36355e820428fbad79c6086ba4a8fa9bd158
Anonymus1145 <Anonymus1145@yahoo.com>
2023-11-03 15:03:07 +0200

    Refactor start() function and winCombo() function in dist/scripts/script.js
    I refactored the start() function to improve readability and remove unnecessary code duplication. I also added a check to ensure that the game is not full before checking for a win.
    
    I refactored the winCombo() function to use a more concise approach using arrays and the includes() method.
    x

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,6 +1,14 @@
 //Game Board
 const gamepad = {
   gameBoard: [],
+  isFull: function () {
+    if (this.gameBoard.length === 9) {
+      alert("Game is Over!");
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 // Player Object
 const player = {
@@ -8,10 +16,63 @@ const player = {
   type: "",
   symbol: "",
   alertWin: function () {
-    alert(this.type + "win!");
+    alert(this.type + " win!");
   },
   random: function () {
     return Math.floor(Math.random() * 9);
+  },
+  winCombo: function () {
+    if (
+      this.markedCells.includes("square1") &&
+      this.markedCells.includes("square2") &&
+      this.markedCells.includes("square3")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square4") &&
+      this.markedCells.includes("square5") &&
+      this.markedCells.includes("square6")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square7") &&
+      this.markedCells.includes("square8") &&
+      this.markedCells.includes("square9")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square1") &&
+      this.markedCells.includes("square4") &&
+      this.markedCells.includes("square7")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square2") &&
+      this.markedCells.includes("square5") &&
+      this.markedCells.includes("square8")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square3") &&
+      this.markedCells.includes("square6") &&
+      this.markedCells.includes("square9")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square1") &&
+      this.markedCells.includes("square5") &&
+      this.markedCells.includes("square9")
+    ) {
+      return true;
+    } else if (
+      this.markedCells.includes("square3") &&
+      this.markedCells.includes("square5") &&
+      this.markedCells.includes("square7")
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   },
 };
 
@@ -34,25 +95,43 @@ function start() {
     dialog.close();
 
     const player1 = Object.create(player);
-    player1.type = "Player";
+    player1.type = "Player 1";
     player1.symbol = "X";
 
     const player2 = Object.create(player);
-    player2.type = "Player";
+    player2.type = "Player 2";
     player2.symbol = "O";
 
+    let currentPlayer = "Player 1";
 
     for (let i = 0; i <= 9; i++) {
       document.addEventListener("click", (event) => {
         if (event.target === document.getElementById("square" + [i])) {
-          player1.markedCells.push("square" + [i]);
-          gamepad.gameBoard.push("square" + [i]);
-
-          document.getElementById("square" + [i]).innerHTML = player1.symbol;
-
-          console.log("square" + [i]);
-          console.log(gamepad.gameBoard);
-          console.log(player1.markedCells);
+          if (currentPlayer === "Player 1") {
+            player1.markedCells.push("square" + [i]);
+            gamepad.gameBoard.push("square" + [i]);
+            document.getElementById("square" + [i]).innerHTML = player1.symbol;
+              if (gamepad.isFull() === true) {
+                if (player1.winCombo() === true) {
+                  player1.alertWin();
+                }
+              }
+          } else {
+            player2.markedCells.push("square" + [i]);
+            gamepad.gameBoard.push("square" + [i]);
+            document.getElementById("square" + [i]).innerHTML = player2.symbol;
+              if (gamepad.isFull() === true) {
+                if (player2.winCombo() === true) {
+                  player2.alertWin();
+                }
+              } 
+          }
+          
+          if (currentPlayer === "Player 1") {
+            currentPlayer = "Player 2";
+          } else {
+            currentPlayer = "Player 1";
+          }
         }
       })
     }
